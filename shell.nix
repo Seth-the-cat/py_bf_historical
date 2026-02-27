881337af16845cde029b2d4382fc0875e1815c60
seth-the-cat <ty@toliveistofly.com>
2026-02-27 01:00:05 -0600

    ok fr fix to require

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
     buildInputs = [
         pkgs.python3
         pkgs.python3Packages.pip
+        pkgs.python3Packages.pip-chill
         pkgs.python3Packages.flask
         pkgs.python3Packages.requests
         pkgs.python3Packages.bleach
